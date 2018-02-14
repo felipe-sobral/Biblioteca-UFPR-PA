3435546bd8ad3b69bb8065de05fce7dfa8a8a281
krepper <krepper@outlook.com>
2018-02-14 19:29:10 -0200

    Teste

--- login.php
+++ login.php
@@ -3,22 +3,39 @@
 
     $user = $_POST['usuario'];
     $pass = $_POST['senha'];
+    $funcao = $_POST['funcao'];
 
     $sql = mysqli_query($conectar, "SELECT * FROM usuarios WHERE usuario = '{$user}' AND senha = '{$pass}'") or die (mysql_error());
 
-    $dado = mysql_fetch_array($sql);
+    $dado = mysqli_fetch_array($sql);
     $resultado = mysqli_num_rows($sql);
 
-    if(resultado == 0){
-        echo 0;
-    } else {
-        echo 1;
-        if(!isset($_SESSION)){
+    $retorno = 0;
+
+    if($funcao == 0){
+        if($resultado == 0){
+            $retorno = 0;
+        } else {
+            if(!isset($_SESSION)){
+                session_start;
+                $_SESSION['id'] = $dado['id'];
+                $_SESSION['nome'] = $dado['nome'];
+                $_SESSION['usuario'] = $dado['usuario'];
+                $_SESSION['nivel'] = $dado['nivel'];
+            }
+            $retorno = 1;
+        } 
+    }
+
+    if($funcao == 1){
+        if($isset($_SESSION)){
             session_start;
-            $_SESSION['id'] = $dado['id'];
-            $_SESSION['usuario'] = $dado['usuario'];
-            $_SESSION['nivel'] = $dado['nivel'];
-            exit;
+            echo $_SESSION['nome'];
+        } else {
+            echo "USUÁRIO NÃO LOGADO";
         }
-    } 
+    }
+
+    echo $retorno;
+    exit;
 ?>
