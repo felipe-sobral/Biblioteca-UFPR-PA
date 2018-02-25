ba184612d0e2f947bb66586248e506942f3f39cc
Felipe Sobral <krepper@outlook.com>
2018-02-25 03:28:20 -0300

    Pagina area_restrita

--- area_restrita.php
+++ area_restrita.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
 
+    $executarFuncao = $_POST['executarFuncao'];
+
     function login_erro(){
         header("Location: login.html");
     }
@@ -21,6 +23,12 @@
     if($login_check == 0){
         login_erro();
     }
+
+    if(executarFuncao == 1){
+        if($nivel>0){
+            echo $nivel;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -32,10 +40,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <script src="js/main.js"></script>
-    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-3.3.1.js"></script>
 
     <link rel="stylesheet" href="css/main.css"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
 </head>
 <body>
     <div class="container-fluid barraCima" style="text-align: right">
@@ -43,16 +51,70 @@
         
     </div>
 
-    <div class="container-fluid cabecalho">
-        <h1 style="filter: invert();">BIBLIOTECA UFPR</h1>
+    <div class="container-fluid cabecalho text-light">
+        <h1>BIBLIOTECA UFPR</h1>
     </div>
 
-    <h2>Bem vindo a área restrita!!!</h2>
+    <div id="botoes" class="container-fluid" style="padding-top: 2%">
+        <div id="botaoRegistrar">
+            <button id="registrarUsuario" type="button" class="btn btn-dark">Registra Usuário</button>
+
+            <div id="registrarUsuarioDiv">
+                <center>
+                <form style="padding-top: 5%; width: 35%;">
+                    <h1>REGISTRAR USUÁRIO</h1>
+                    <div class="form-group">
+                        <label for="usuario">Usuário</label>
+                        <input type="text" class="form-control" id="usuario" placeholder="Ex: maria">
+                    </div>
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome" placeholder="Ex: Maria">
+                    </div>
+                    <div class="form-group">
+                        <label for="senha">Senha</label>
+                        <input type="text" class="form-control" id="senha" placeholder="Senha">
+                    </div>
+                    <div class="form-group">
+                        <label for="nivel">Nível do usuário</label>
+                        <input type="number" class="form-control" id="nivel" placeholder="Min: 1 ~ Max: 5" min="1" max="5">
+                    </div>
+               
+                    <button type="submit" class="btn btn-primary">Registrar</button>
+                </form>
+                </center>
+            </div>
+
+        </div>
+    </div>
 
     <div class="container-fluid footer text-light" style="text-align: center">
         UFPR Biblioteca PA
         
     </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#registrarUsuario").attr("disabled","disabled");
+            $.ajax({
+                url: "area_restrita.php",
+                type: "post",
+                data: "executarFuncao="+1,
+                success: function(result){
+                    if(result==5){
+                        $("#registrarUsuario").attr("enable","enable");
+                        
+                    }
+                }
+            
+            })
+            
+        })
+        
+        $("#registrarUsuario").click(function() {
+            $("#registrarUsuarioDiv").css("display", "block");
+        });
+    </script>
 
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ function procurar(){
         var data = new Date();
         alert(data.toString());
 }
+
+
