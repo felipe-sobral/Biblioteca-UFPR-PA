ca60fe9992bcb770b9fe9001c99ab3b794f02d40
krepper <krepper@outlook.com>
2018-03-02 12:00:07 -0300

    Index Atualizado

--- buscarLivro.php
+++ buscarLivro.php
@@ -4,20 +4,43 @@
 	include "cfg.php";
 
 	$codigo = $_POST['barra'];
+	$item = $_POST['item'];
 
-	$sql = mysqli_query($conectar, "SELECT * FROM livros WHERE barra = '{$codigo}' AND ativo = '1'") or die (mysql_error());
+	$sql = mysqli_query($conectar, "SELECT * FROM livros WHERE barra='{$codigo}' OR codigo='{$codigo}' AND ativo = '1'") or die (mysql_error());
 	$check = mysqli_num_rows($sql);
 
 	$retorno = 0;
-        
+
     if($check == 0){
-    	
+
     	$retorno = 0;
 
     } else {
     	$dado = mysqli_fetch_array($sql);
 
-    	$retorno = $dado['estante'];
+			switch ($item) {
+				case 0:
+					$retorno = $dado['estante'];
+					break;
+
+				case 1:
+					$retorno = $dado['nome'];
+					break;
+
+				case 2:
+					$retorno = $dado['link'];
+					break;
+
+				case 3:
+					$retorno = $dado['barra'];
+					break;
+
+				case 4:
+					$retorno = $dado['codigo'];
+					break;
+
+			}
+
 
     }
 
