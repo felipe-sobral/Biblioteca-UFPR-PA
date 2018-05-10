ebfa20e48389e65832d00beb1cbf52705880bc6a
Felipe Sobral <krepper@outlook.com>
2018-05-10 11:29:47 -0300

    Delete cfg.php

--- cfg.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  define('BD_USER', 'u428639742_felip');
-  define('BD_PASS', 'rootfs');
-  define('BD_NAME', 'u428639742_bibli');
-	$conectar = mysqli_connect('Localhost', BD_USER, BD_PASS, BD_NAME);
-
-?>
