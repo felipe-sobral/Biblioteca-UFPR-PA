c3061f5b74996cd9b6a349bbb232fbb47091dbeb
Felipe Sobral <krepper@outlook.com>
2018-05-16 10:00:30 -0300

    + HISTORICO CONSULTA LOCAL

--- livro_est/historico_registros.php
+++ livro_est/historico_registros.php
@@ -4,17 +4,22 @@
   // FN 2 == LISTAR TOTAL DE LIVROS, IGUAL NO CONSULTA LOCAL
 
   include "../cfg.php";
+  include "../funcoesGerais";
+
+  function contarLivrosDia($ano, $mes, $dia){
+    $sql = mysqli_query($GLOBALS['conectar'], "SELECT * FROM consulta_local WHERE ano='{$ano}' AND mes='{$mes}' AND dia='{$dia}'");
+    if(verificarSql($sql)){
+
+    }
+  }
 
   if(isset($_POST['fn']) && $_POST['fn'] == 1){
-    $sql_ano = mysqli_query($conectar, "SELECT YEAR(CURRENT_TIMESTAMP)");
-    $sql_mes = mysqli_query($conectar, "SELECT MONTH(CURRENT_TIMESTAMP)");
-    $sql_dia = mysqli_query($conectar, "SELECT DAY(CURRENT_TIMESTAMP)");
+    $ano = retornaData(mysqli_query($conectar, "SELECT YEAR(CURRENT_TIMESTAMP)"));
+    $mes = retornaData(mysqli_query($conectar, "SELECT MONTH(CURRENT_TIMESTAMP)"));
+    $dia = retornaData(mysqli_query($conectar, "SELECT DAY(CURRENT_TIMESTAMP)"));
 
-    $ano = mysqli_fetch_row($sql_ano); $ano = $ano[0];
-    $mes = mysqli_fetch_row($sql_mes); $mes = $mes[0];
-    $dia = mysqli_fetch_row($sql_dia); $dia = $dia[0];
 
-    $sql = mysqli_query($conectar, "SELECT * FROM consulta_local WHERE")
+
   }
 
 ?>
