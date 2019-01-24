1f12a115aa57f1bb4784fe067a97efb2feaddd92
krepper <krepper@outlook.com>
2019-01-24 11:55:57 -0200

    FIX

--- root/construtor/funcoes/sessao/autenticacao.php
+++ root/construtor/funcoes/sessao/autenticacao.php
@@ -1,8 +1,6 @@
 <?php
-   /**
-    * CORRIGIR
-    */
    require_once $_SERVER["DOCUMENT_ROOT"]."/root/init.php";
+   require_once QUERY;
 
    /**
     * 
@@ -45,6 +43,29 @@
          if($_SESSION["usr_nivel"] >= $nivel){
             return true;
          }
+      }
+
+      return false;
+   }
+
+   /**
+    * Retorna informações do usuário passado caso contenha no banco de dados.
+    *
+    * @param  string $usuario
+    * @param  string $senha
+    *
+    * @return bool|array
+    */
+    function usuario($usuario, $senha){
+      $consulta = new Query;
+
+      $vUsuario = $consulta->valor("usuario", "$usuario");
+      $vSenha = $consulta->valor("senha", "$senha");
+
+      $consulta->selecionar("usuarios", "*", "usuario = $vUsuario AND senha = $vSenha");
+
+      if($consulta->executar()){
+         return $consulta->assoc_array();
       }
 
       return false;
